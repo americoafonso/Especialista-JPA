dc9aed60cb5589e5a2e4b0027dc0278e3908dea6
americo afonso <afonsoamericoafonso@gmail.com>
2020-12-16 20:18:31 -0300

    Creating a generic class for testing.

---
+++ src/test/java/com/algaworks/ecommerce/EntityManagerTest.java
@@ -0,0 +1,34 @@
+package com.algaworks.ecommerce;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EntityManagerTest {
+
+    protected static EntityManagerFactory entityManagerFactory;
+
+    protected EntityManager entityManager;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
+    }
+
+    public static void tearDownAfterClass() {
+        entityManagerFactory.close();
+    }
+
+    @Before
+    public void setUp() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    public void tearDown() {
+        entityManager.close();
+    }
+
+}
---
+++ src/test/java/com/algaworks/ecommerce/iniciandoComJpa/ConsultandoRegistrosTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.ecommerce.iniciandoComJpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Produto;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ConsultandoRegistrosTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPorIdentificador() {
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Assert.assertNotNull(produto);
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+    public void atualizarReferencia() {
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setNome("Microfone Samson");
+
+        entityManager.refresh(produto);
+
+        Assert.assertEquals("Kindle", produto.getNome());
+    }
+
+}
